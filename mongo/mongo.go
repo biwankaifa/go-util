8ba9620215ea98e4f331271988b17e2d6ae9b975
顾龙飞 <873527492@qq.com>
2021-08-31 15:35:14 +0800

    mongo

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -2,77 +2,38 @@ package mongo
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"github.com/biwankaifa/go-util/config"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const MaxConnectionNum = 500
+var databaseOfMongo *mongo.Database
 
-var cfg = config.Get().MongoDb
-
-type Connection struct {
-	Used   bool
-	Pos    int
-	Client *mongo.Client
+type ConfigOfMongo struct {
+	Host     string
+	Port     int
+	Name     string
+	Pass     string
+	DataBase string
 }
 
-func (c *Connection) TryConnection() (*mongo.Database, error) {
-	err := c.Client.Ping(context.Background(), nil)
-	if err != nil {
-		return nil, err
+func InitMongo(cfg ConfigOfMongo) error {
+	if databaseOfMongo != nil {
+		return nil
 	}
-	return c.Client.Database(cfg.DataBase), err
-}
-
-type ConnectionPool struct {
-	ClientList []Connection
-}
-
-var CP = ConnectionPool{}
-
-func GetClient() (*Connection, error) {
-	for i := 0; i < len(CP.ClientList); i++ {
-		if CP.ClientList[i].Used == false {
-			CP.ClientList[i].Used = true
-			return &CP.ClientList[i], nil
-		}
-	}
-	pos := len(CP.ClientList)
-	if pos >= MaxConnectionNum {
-		return nil, errors.New("Mongo连接池连接量不足")
-	}
-	database, err := DbConnect()
-	if err != nil {
-		return nil, err
-	}
-	connection := Connection{
-		Used:   false,
-		Pos:    pos,
-		Client: database,
-	}
-
-	CP.ClientList = append(CP.ClientList, connection)
-	return &connection, nil
-}
-func Release(pos int) {
-	CP.ClientList[pos].Used = false
-}
-
-func DbConnect() (*mongo.Client, error) {
 	var param string
-	if cfg.String != "" {
-		param = cfg.String
-	} else {
-		param = fmt.Sprintf("mongodb://%s:%s@%s:%d", cfg.Name, cfg.Pass, cfg.Host, cfg.Port)
-	}
-
+	param = fmt.Sprintf("mongodb://%s:%s@%s:%d", cfg.Name, cfg.Pass, cfg.Host, cfg.Port)
 	clientOptions := options.Client().ApplyURI(param)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return client, nil
+
+	databaseOfMongo = client.Database(cfg.DataBase)
+
+	return nil
+}
+
+func GetMongoDatabase() *mongo.Database {
+	return databaseOfMongo
 }
